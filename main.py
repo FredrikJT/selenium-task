3cf48e1e3b34684a4bbf27240474aa28e3170826
Fredrik <fredrik.j.torneus@gmail.com>
2019-06-18 17:58:17 +0200

    Outline of main file, added TODO:s which should cover the function of the task

---
+++ main.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jun 18 17:54:26 2019
+
+@author: fj3279
+"""
+
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
+#TODO: Navigates to https://sprint.tobiipro.com
+
+#TODO: Log in using the credentials
+
+#TODO: Import credentials from local file
+
+#TODO: Verify that the log in was successful
+
+#TODO: Log out
